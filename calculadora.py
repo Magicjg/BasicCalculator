773920b83a3be75fa00251699f5bdcc1d0fee0db
Alan Juarez <112911836+Magicjg@users.noreply.github.com>
2022-10-08 19:33:44 -0500

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,44 @@
+#Calculator v1 
+
+import os
+
+
+    
+print('1)Addition')
+print('2)Subtraction')
+print('3)Multiply')
+print('4)Split')
+print('5) exit')
+print('')
+    
+
+dec = str(input('Choose an option: '))
+
+os.system("clear")
+
+if dec=='1':
+        num1 = float(input('Insert your first number: '))
+        num2 = float(input('Insert your second number: '))
+        res = num1 + num2
+        print('Result:  '+ str(res))
+elif dec=='2':
+        num1 = float(input('Insert your first number: '))
+        num2 = float(input('Insert your second number: '))
+        res = num1 - num2
+        print('Result:  ' + str(res))
+elif dec=='3':
+        num1 = float(input('I5nsert your first number: '))
+        num2 = float(input('insert your second number: '))
+        res = num1 * num2
+        print('Result:  ' + str(res))
+elif dec=='4':
+        num1 = float(input('insert your first number:'))
+        num2 = float(input('insert your second number: '))
+        res = num1 / num2 
+        print('Result: '+ str(res))
+elif dec=='5':
+        print('BYE')
+        
+else:
+       print('BYE') 
+
